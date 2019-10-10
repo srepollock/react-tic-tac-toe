5d52d628637ae267d00bd112b62e9718ba04d6a1
Spencer Pollock <srepollock@gmail.com>
2019-10-10 14:42:15 -0700

    Updates

---
+++ precache-manifest.72df84eb995107c425e97ac6eeb03272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207bc88d98c216cf535864c0d494230e",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f33b0e6491d49d5434e7",
+    "url": "/react-tic-tac-toe/static/css/main.851bd3f0.chunk.css"
+  },
+  {
+    "revision": "20f9d640b35f9f7178c5",
+    "url": "/react-tic-tac-toe/static/js/2.57ab53ec.chunk.js"
+  },
+  {
+    "revision": "f33b0e6491d49d5434e7",
+    "url": "/react-tic-tac-toe/static/js/main.95e1664f.chunk.js"
+  },
+  {
+    "revision": "89eaaeb9b8e50146e165",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.a1646505.js"
+  }
+]);
